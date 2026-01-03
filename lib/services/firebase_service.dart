2916cdf641b32cfe25f9df8d03f2833e6c6c41b0
Angel-cloud8 <malekayachi440@gmail.com>
2026-01-03 12:32:28 +0100

    Create firebase_service.dart

---
+++ lib/services/firebase_service.dart
@@ -0,0 +1,43 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class FirebaseService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<bool> login(String email, String password) async {
+    try {
+      await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return true;
+    } catch (e) {
+      print(e);
+      return false;
+    }
+  }
+
+  Future<bool> register(String email, String password, String name, String role) async {
+    try {
+      UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      /*await FirebaseFirestore.instance
+          .collection('users')
+          .doc(userCredential.user!.uid)
+          .set({
+        'uid': userCredential.user!.uid,
+        'email': email.trim(),
+        'name': name,
+        'role': role,
+        'approved': role == 'student' ? false : true,
+        'createdAt': FieldValue.serverTimestamp(),
+      });
+*/
+      return true;
+    } catch (e) {
+      print(e);
+      return false;
+    }
+  }
+}
